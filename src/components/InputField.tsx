8397ff779e9b740eca6f42f35d85fc7b9b8271d2
Gloria Villa <hello.gloria@gmail.com>
2024-03-17 18:17:55 -0700

    added task submit form

---
+++ src/components/InputField.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./styles.css";
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  //handleAdd:() => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+  return (
+    <form className="input">
+      <input type="input" 
+        value={todo}
+        onChange={
+          (e)=>setTodo(e.target.value)}
+          placeholder="Enter a task" 
+          className="input_box"
+      />
+      <button className="input__submit" type="submit">Go</button>
+    </form>
+    );
+};
+
+export default InputField;
+     
