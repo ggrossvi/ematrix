2b8f205f7903be5b7b9ffbd096abd752677473c1
Gloria Villa <hello.gloria@gmail.com>
2024-03-17 22:10:20 -0700

    added todos css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useState} from "react";
 import './App.css'
 import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
+import { Todo } from "./components/model";
 
 const App: React.FC = () => {
     const [todo, setTodo] = useState<string>("");
@@ -21,11 +23,8 @@ const App: React.FC = () => {
       <div className="App">
       <span className = "heading">EMatrix</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
-      {/*<TodoList/>*/}
-      {todos.map((t) => (
-        <li>{t.todo}</li>
-
-      ))}
+      <TodoList todos={todos} setTodos={setTodos}/>
+    
     </div>
     );
 }
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Todo } from "../model";
+import "./styles.css";
+
+interface Props {
+    todos: Todo[];
+    setToDos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const TodoList: React.FC<Props> = ({todos, setTodos}) => {
+  return (
+    <div className="todos">
+        {todos.map((todo) => (
+            <li>{todo.todo}</li>
+        ))}
+
+    </div>
+  );  
+  
+};
+
+export default TodoList;
